0b1e6d3ec60486c649af3c4a1869ee62c7297cf1
iryawan91 <iryawan1991@gmail.com>
2022-06-04 16:55:51 +0700

    Tipe Data pada PHP

---
+++ tipedata.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 'ini adalah string'; //string
+$b = 10; //integer
+$c = 10.5; //float
+$d = true; //boolean
+$e= [
+    'satu',
+    'dua',
+    'tiga'
+]; //array
+$f = NULL;
+
+echo var_dump($f);
