137a906d71671f6f22db4487e2a94086b9fd24da
iryawan91 <iryawan1991@gmail.com>
2022-06-05 09:43:17 +0700

    Do While

--- looping.php
+++ looping.php
@@ -2,8 +2,17 @@
 //Looping akan berjalan ketika kondisi true
 
 $a=1;
+$b=1;
 
 while($a <= 15){
     echo 'Looping While saya bernilai : ' .$a. "</br>";
     $a++;
 }
+
+//do while akan berjalan dahulu  teerus dicek kondisi nya
+
+
+do{
+    echo 'Looping Do-While saya bernilai : ' .$b. "</br>";
+    $b++;
+}while($b <= 10);
